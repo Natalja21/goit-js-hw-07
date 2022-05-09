c1f756f9ac0b865b761bb9dfceea2e5c53aed31c
Natalia Kozak <90988795+Natalja21@users.noreply.github.com>
2022-05-09 15:11:55 +0300

    hw-js-07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+function makeGalleryMarkup() {
+    const markup = galleryItems
+        .map(({ preview, original, description }) =>
+            `<div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}">
+            </a>
+        </div>`
+        )
+        .join('');
+
+    galleryEl.innerHTML = markup;
+};
+
+makeGalleryMarkup();
+
+galleryEl.addEventListener('click', onBasicLightboxOpen);
+
+function onBasicLightboxOpen(e) {
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    };
+
+    e.preventDefault();
+
+    const modalImg = basicLightbox
+        .create(`<img src="${e.target.dataset.source}" alt="${e.target.alt}">`);
+
+    modalImg.show();
+
+    window.addEventListener('keydown', e => {
+        if (e.code === 'Escape' && modalImg.visible()) {
+            modalImg.close();
+        }
+    });
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+makeGalleryMarkup();
+
+const lightbox = new SimpleLightbox('.gallery__item', { captionsData: 'alt', captionDelay: 250});
+
+function makeGalleryMarkup() {
+    const markup = galleryItems
+        .map(({ preview, original, description }) =>
+            `<a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}">
+        </a>`
+        )
+        .join('');
+    galleryEl.innerHTML = markup;
+};
